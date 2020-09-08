0437a20d2e7e50521218e08039e4417ebbe98577
Anpilogoff <aa.anpilogov@rambler.ru>
2020-09-08 05:51:44 +0300

    'after_CP_checkpoint'

--- src/main/java/com/anpilogoff/controller/servlets/LoginServlet.java
+++ src/main/java/com/anpilogoff/controller/servlets/LoginServlet.java
@@ -1,10 +1,11 @@
 package com.anpilogoff.controller.servlets;
 
+import com.anpilogoff.model.connection.ConnectionBuilder;
+import com.anpilogoff.model.connection.DBconnector;
 import com.anpilogoff.model.dao.Dao;
 import com.anpilogoff.model.dao.UserDAO;
 import com.anpilogoff.model.entity.UserCredentials;
 import org.apache.log4j.Logger;
-import org.w3c.dom.UserDataHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -15,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * receive req/resp object from
@@ -29,6 +32,7 @@ import java.io.IOException;
 @WebServlet
 public class LoginServlet extends HttpServlet {
   private static final  Logger log = Logger.getLogger(LoginServlet.class);
+  private UserDAO dao;
 //    /**
 //     * @param request  is object created by Tomcat after request receiving and initialized with request attributes
 //     * @param response is empty-object created by Tomcat after request receiving(will be initialized when method
@@ -38,6 +42,12 @@ public class LoginServlet extends HttpServlet {
 //        request.getRequestDispatcher("login.html").forward(request,response);
 //    }
 
+    @Override
+    public void init() throws ServletException {
+        this.dao = new UserDAO();
+        dao.setConnectionBuilder(new DBconnector());
+    }
+
     /**
      * @param req  is object created by Tomcat after request receiving and initialized with request attributes
      * @param resp is empty-object created by Tomcat after request receiving(will be initialized when method
@@ -46,21 +56,22 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-        System.out.println("login");
                 if (!req.getParameter("login").equals("") && !req.getParameter("password").equals("")) {
                     String login = req.getParameter("login");
                     String password = req.getParameter("password");
-                    System.out.println("params: "+ login + " " + password);
 
                     //todo: dao logic|| comparing parameters with data in db and if compare ++ ->
                     //todo: +++ encrypt password
-                    HttpSession session = req.getSession(true);
 
+                    HttpSession session = req.getSession(true);
                     UserCredentials credentials = new UserCredentials(login, password);
                     session.setAttribute("credentials", credentials);
-                    resp.sendRedirect( req.getServletContext().getContextPath() +"/userhome");
+                    dao.registerNewUser();
+                    resp.sendRedirect( "userhome");
                 } else {
                     resp.sendRedirect("login");
                 }
             }
-        }
+
+
+}
--- src/main/java/com/anpilogoff/model/dao/Dao.java
+++ src/main/java/com/anpilogoff/model/dao/Dao.java
@@ -1,8 +1,14 @@
 package com.anpilogoff.model.dao;
 
+import com.anpilogoff.model.entity.User;
+import sun.java2d.cmm.Profile;
+
+import java.sql.Connection;
+
 public interface Dao{
-    void registerNewUser();
-    void registerNewProfile();
-    void deleteUser();
-    void blockUser();
+    User registerNewUser(String login, String password);
+    Profile registerNewProfile();
+    boolean deleteUser();
+    boolean blockUser();
+
 }
